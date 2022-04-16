adbd8e8a6d50020148eb364f7e39065b9729593a
Amiya <anl3488@nyu.edu>
2022-04-16 16:50:17 -0400

    Enemy AI moving

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,38 +5,42 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     private Animator animator;
+    Rigidbody rigid;
 
     [SerializeField] float speed = 4.0f;
     [SerializeField] float gravity = 20.0f;
-    [SerializeField] bool isXmoving;
+    [SerializeField] bool isXmoving;        // checks if it is moving "horizontally"
 
     private Vector3 moveDirection = Vector3.zero;
 
-    private float xSpeed;
-    private float ySpeed;
+    // x and y speed values
+    private float xSpeed = 4.0f;
+    private float ySpeed = 4.0f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();      // referencing enemy's rigidbody
 
+        // checking for which way i say in the editor to move
         if (isXmoving)
         {
-            ySpeed = 0;
+            ySpeed = 0.0f;
         }
         else
         {
-            xSpeed = 0;
+            xSpeed = 0.0f;
         }
     }
 
     private void FixedUpdate()
-    {
+    {/*
         moveDirection = new Vector3(xSpeed, 0.0f, ySpeed);
         moveDirection *= speed;
         transform.position = moveDirection;
-
+        */
         // Apply gravity
-        moveDirection.y -= gravity * Time.fixedDeltaTime;
+        //moveDirection.y -= gravity * Time.fixedDeltaTime;
     }
     
     void Update()
@@ -47,13 +51,26 @@ public class EnemyBehavior : MonoBehaviour
         transform.position = moveDirection;
         */
 
+        rigid.velocity = new Vector3(xSpeed, 0, ySpeed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ebounds")
         {
-            speed *= -1;
+            //speed *= -1;
+            xSpeed *= -1;
+            ySpeed *= -1;
+
+            if(speed < 0)
+            {
+                animator.SetBool("reverse", true);
+            }
+            else
+            {
+                animator.SetBool("reverse", false);
+            }
+            
         }
     }
 }
