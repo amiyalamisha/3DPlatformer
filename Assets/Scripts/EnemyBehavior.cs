e0f9f0d6bc77e3c4fc564298ac187935f7954523
Amiya <anl3488@nyu.edu>
2022-04-16 16:23:51 -0400

    Enemy behavior

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+
+    [SerializeField] float speed = 4.0f;
+    [SerializeField] float gravity = 20.0f;
+    [SerializeField] bool isXmoving;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    private float xSpeed;
+    private float ySpeed;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+        if (isXmoving)
+        {
+            ySpeed = 0;
+        }
+        else
+        {
+            xSpeed = 0;
+        }
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        moveDirection = new Vector3(xSpeed, 0.0f, ySpeed);
+        moveDirection *= speed;
+        transform.position = moveDirection;
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+    }
+    
     void Update()
     {
-        
+        /*
+        moveDirection = new Vector3(xSpeed, 0.0f, ySpeed);
+        moveDirection *= speed;
+        transform.position = moveDirection;
+        */
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ebounds")
+        {
+            speed *= -1;
+        }
     }
 }
